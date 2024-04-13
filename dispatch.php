0459f57f0fddf7aa57521aff3f69b55a5eace6c3
joey2310 <bhagatpratik23@gmail.com>
2024-04-14 02:51:40 +0530

    small changes

--- dispatch.php
+++ dispatch.php
@@ -57,7 +57,7 @@
 <br>
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+  Add New Material for Dispatch
 </button>
 
 <!-- Modal -->
@@ -65,15 +65,30 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Material for Dispatch</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+      <div >
+        <form name="form" action="dispatch1.php" method="POST">
+            <div class="mb-3">
+            <input type="date" id="user" name="user" placeholder="Date" required><br><br>
+            </div>
+            <div class="mb-3">
+            <input type="number" id="num1" name="num1" placeholder="#" required><br><br>
+            </div>
+            <div class="mb-3">
+            <input type="number" id="sap" name="sap" placeholder="Sap Code" required><br><br>
+            </div>
+            <div class="mb-3">    
+            <input type="number" id="disqty" name="disqty" placeholder="Dispatch Quantity" required><br><br>
+            </div>
+        </form>
+    </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Add</button>
       </div>
     </div>
   </div>
@@ -81,6 +96,7 @@
 <button type="button" class="btn btn-outline-primary">View Dispatch List</button>
 <br>
 <br>
+<div class="table-responsive-sm">
 <table class="table table-hover table-sm">
   <thead>
     <tr>
@@ -96,6 +112,7 @@
       <th scope="col">Dispatch Total</th>
       <th scope="col">Pending</th>
       <th scope="col">Description</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -112,6 +129,9 @@
       <td>13</td>
       <td>1</td>
       <td>Idler Pulley Shaft</td>
+      <td><button class="btn btn-success btn-primary btn-sm" type="submit" name="submit">Update</button>
+        <a class="btn btn-info btn-danger btn-sm" type="submit" name="cancel">Delete</a><br>
+        </td>
     </tr>
     <tr>
       <th scope="row">2</th>
@@ -126,6 +146,9 @@
       <td>18</td>
       <td>6</td>
       <td>Ring For Idler Pulley</td>
+      <td><button class="btn btn-success btn-primary btn-sm" type="submit" name="submit">Update</button>
+        <a class="btn btn-info btn-danger btn-sm" type="submit" name="cancel">Delete</a><br>
+        </td>
     </tr>
     <tr>
       <th scope="row">3</th>
@@ -140,9 +163,13 @@
       <td>4</td>
       <td>8</td>
       <td>Washer For Idler Pulley</td>
+      <td><button class="btn btn-success btn-primary btn-sm" type="submit" name="submit">Update</button>
+        <a class="btn btn-info btn-danger btn-sm" type="submit" name="cancel">Delete</a><br>
+        </td>
     </tr>
   </tbody>
 </table>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
