5a6317ad18a32ba54da368fd3fcc3441bd1823da
joey2310 <bhagatpratik23@gmail.com>
2024-04-15 00:48:35 +0530

    login to db

--- login.php
+++ login.php
@@ -12,27 +12,17 @@
 <body>
     <div id = "form">
         <h1>Login Form</h1>
-        <form name = "form" action="login1.php" onsubmit="return isvalid()" method ="POST">
+        <form name = "form" action="login1.php" method ="POST">
         <label>Username: </label>
-        <input type="text" id="user" name="user"></input>
+        <input type="text" id="user" name="username"></input>
         <br>
         <br>
         <label>Password: </label>
-        <input type="password" id="pass" name="pass"></input>
+        <input type="password" id="pass" name="password"></input>
         <br>
         <br>
         <input type="submit" id="btn" value="Login" name="submit"></input>
     </form>
 </div>
-<script>
-    function isvalid(){
-        var user = document.form.user.value;
-        var user = document.form.pass.value;
-        if(user.length ==""&& pass.length==""){
-            alert("Username / Password field is emtpy !!!");
-            reutn false;
-        }
-    }
-</script>
 </body>
 </html>
--- login1.php
+++ login1.php
@@ -1,10 +1,10 @@
 <?php
     include("connection.php");
     if(isset($_POST['submit'])){
-        $username =  $_POST['user'];
-        $password = $_POST['pass'];
+        $username =  $_POST['username'];
+        $password = $_POST['password'];
 
-        $sql = "select * from login where username = '$username' and password = '$password'";
+        $sql = "select * from signup where username = '$username' and password = '$password'";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
